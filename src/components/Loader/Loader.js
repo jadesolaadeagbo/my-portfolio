9bf088a0234a7c6b8ba526b367beb5841853e64f
Rubies21 <jadesolaadeagbo@gmail.com>
2023-03-21 22:17:10 +0100

    First portfolio commit

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoaderContainer from "../../containers/LoaderContainer";
+import Mehdi from "../../assets/images/Mehdi";
+
+const Loader = () => {
+    return (
+        <LoaderContainer>
+            <h1 style={{color:"#cc2b5e", border:'1px solid #cc2b5e', padding: '10px',}}>JADESOLA ADEAGBO</h1>
+        </LoaderContainer>
+    ); 
+};
+
+export default Loader;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+const skillsList = [
+    { title: "React JS", value: 75 },
+    // { title: "Next js", value: 80 },
+    { title: "Material UI", value: 80 },
+    { title: "Styled components", value: 80 },
+    { title: "Node JS ", value: 60 },
+    // { title: "Express JS ", value: 70 },
+    // { title: "Firebase", value: 70 },
+];
+const projectList = [
+    {
+        id: 1,
+        title: "Afriverse",
+        technologies: ["HTML","CSS","React JS", "Node JS"],
+        backgroundImage:
+            "https://cdn.cbeditz.com/cbeditz/preview/black-red-gradient-background-wallpaper-74-11614352798fbqrv1wpuv.jpg",
+        frontImage: "https://i.postimg.cc/PxThJhGT/afriverse.png" ,
+    },
+    {
+        id: 2,
+        title: "Loruki Website",
+        technologies: ["HTML", "CSS", "Material UI"],
+        backgroundImage:
+            "https://img.freepik.com/free-vector/dark-gradient-background-with-copy-space_53876-99548.jpg?size=626&ext=jpg&ga=GA1.2.2102900112.1628985600",
+        frontImage: "https://i.postimg.cc/2jqm50Mq/loruki.png",
+    },
+    {
+        id: 3,
+        title: "Frontend Bootcamp",
+        technologies: ["React JS", "Node JS", "Bootstrap"],
+        backgroundImage:
+            "https://media.istockphoto.com/vectors/abstract-purple-vector-background-with-stripes-vector-id972475894?k=6&m=972475894&s=612x612&w=0&h=99AirGMOb64N2-1ZSMYRjEBp2USrAdzXUGzQMh5o6Js=",
+        frontImage: "https://i.postimg.cc/8cL06fLY/bootcamp.png",
+    },
+];
+
+const experienceList = [
+    {
+        id: 0,
+        company: "HNG Internships",
+        links: {
+            website: "https://training.zuri.team",
+            
+        },
+    },
+    {
+        id: 1,
+        company: "Zuri Internships",
+        links: {
+            website: "https://training.zuri.team",
+            instagram: "https://www.instagram.com/zuriinternship",
+        },
+    },
+];
+
+export { skillsList, projectList, experienceList };
